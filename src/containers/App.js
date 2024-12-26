f8a39961534fd992ece2f6e86a72ebd75f624290
zyphr007 <suyashg720@gmail.com>
2024-12-26 05:39:49 +0530

    App.js modified

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import CardList from "../components/CardList";
 import SearchBox from "../components/SearchBox";
 import Scroll from "../components/Scroll";
+import { robots } from "../robots";
 
 class App extends Component {
   constructor() {
